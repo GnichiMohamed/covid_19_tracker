ecc78c6c9a16b0d9d3d16dbd1588131f2caf9ea3
Mohamed_Gnichi <gnichimohamed@gmail.com>
2020-04-17 23:12:03 +0100

    Updates

---
+++ precache-manifest.40874e1899a8ea9682facdd5b293851d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cdb1f41996ce80d811c17a4a0591e45",
+    "url": "/covid_19_tracker/index.html"
+  },
+  {
+    "revision": "3547d991a8810964d132",
+    "url": "/covid_19_tracker/static/css/main.291f8769.chunk.css"
+  },
+  {
+    "revision": "d35a93560e159f0e9460",
+    "url": "/covid_19_tracker/static/js/2.df94c80b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid_19_tracker/static/js/2.df94c80b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3547d991a8810964d132",
+    "url": "/covid_19_tracker/static/js/main.ffaab19c.chunk.js"
+  },
+  {
+    "revision": "ff60d349c45a8cb34f6b",
+    "url": "/covid_19_tracker/static/js/runtime-main.f89f5309.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_19_tracker/static/media/image.d7265326.png"
+  }
+]);
